04124b8113488c382f27ba23e511348e7555362b
Layuntafelipe <flobo0701@icloud.com>
2021-05-24 18:45:47 -0300

    UX done

--- EasyTasks/View/ContentView.swift
+++ EasyTasks/View/ContentView.swift
@@ -65,6 +65,8 @@ struct ContentView: View {
                         Button(action: {
                             //TOGGLE ACTION
                             isDarkMode.toggle()
+                            playSound(sound: "sound-tap", type: "mp3")
+                            feedback.notificationOccurred(.success)
                         }, label: {
                             Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
                                 .resizable()
@@ -79,6 +81,8 @@ struct ContentView: View {
                     //MARK: - NEW TASK BUTTON
                     Button(action: {
                         showNewTaskItem = true
+                        playSound(sound: "sound-ding", type: "mp3")
+                        feedback.notificationOccurred(.success)
                     }, label: {
                         Image(systemName: "plus.circle")
                             .font(.system(size: 30, weight: .semibold, design: .rounded))
@@ -106,10 +110,15 @@ struct ContentView: View {
                     .padding(.vertical,0)
                     .frame(maxWidth: 640)
                 }//VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0.0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
                 
                 //MARK: - NEW TASK ITEM
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ? Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                         .onTapGesture {
                             withAnimation(){
                                 showNewTaskItem = false
@@ -125,7 +134,7 @@ struct ContentView: View {
             }
             .navigationBarTitle("Daily Tasks", displayMode: .large)
             .navigationBarHidden(true)
-            .background(BackgroundImageView())
+            .background(BackgroundImageView().blur(radius: showNewTaskItem ? 8.0 : 0.0, opaque: false))
             .background(backgroundGradient.ignoresSafeArea(.all))
         }//NAVIGATION
         .navigationViewStyle(StackNavigationViewStyle())
